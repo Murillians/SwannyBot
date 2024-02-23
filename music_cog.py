da0ca12dcc13813e148e9806d00cb134492b6eed
Nick Swan <nickswan2624@gmail.com>
2024-02-23 08:27:59 -0500

    Accidentally pushed the old variation of music_cog lol oopsie

--- music_cog.py
+++ music_cog.py
@@ -304,21 +304,6 @@ class MusicCog(commands.Cog):
                 print(e)
                 pass
 
-    # Check every 5 minutes while playing if Swanny Bot is the only member in connected voice channel
-    # If nothing is playing, inactive_player will disconnect instead
-    @commands.Cog.listener()
-    async def empty_channel(self, player: wavelink.Player, ctx: commands.Context) -> None:
-        current_voice_channel = ctx.author.voice.channel
-        await asyncio.sleep(5)
-        if player.playing:
-            print(current_voice_channel.id)
-            print(current_voice_channel.name)
-            print(current_voice_channel.members)
-            print(self.bot.user.id)
-            if current_voice_channel.members == self.bot.get_user(id(self)):
-                await player.disconnect()
-
-
     async def on_wavelink_track_start(self, payload: wavelink.TrackStartEventPayload) -> None:
         player: wavelink.Player | None = payload.player
         if not player:
