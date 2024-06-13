f11b1400448246208b36f9a5a80ebb215a3520c6
Nick Swan <nickswan2624@gmail.com>
2024-06-12 21:57:46 -0400

    Finally have slash command that prompts menu

--- gamedeal_cog.py
+++ gamedeal_cog.py
@@ -12,15 +12,15 @@ swancord = discord.Object(swannybottokens.swancord)
 
 class GameTrackModal(discord.ui.Modal, title="Track Game"):
 
-    # User enters title
-    name = discord.ui.TextInput(
-        label='Title',
-        placeholder='Insert Game Title',
-    )
+    # # User enters title
+    # name = discord.ui.TextInput(
+    #     label='Title',
+    #     placeholder='Insert Game Title',
+    # )
 
     # User enters link with max of 300 characters
     feedback = discord.ui.TextInput(
-        label='Store link to game',
+        label='store.steampowered.com link to game',
         style=discord.TextStyle.long,
         placeholder='Insert Link',
         required=False,
@@ -28,7 +28,11 @@ class GameTrackModal(discord.ui.Modal, title="Track Game"):
     )
 
     async def on_submit(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f'{self.name.value} is now set up for deal alerts!', ephemeral=True)
+        # send steam store link
+        # parse the steamAppID from the link to run through the cheapshark API
+        # Return game deal with 2 buttons, close and track game
+
+        await interaction.response.send_message(f'{self.feedback.value} is now set up for deal alerts!', ephemeral=True)
 
     async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
         await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
@@ -41,25 +45,35 @@ class GameDealCog(commands.Cog, name="GameDealCog"):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.ctx_menu = app_commands.ContextMenu(
-            name="Track Game Deal",
-            callback=self.track_game
-        )
-        self.bot.tree.add_command(self.ctx_menu)
 
-    # @commands.hybrid_command(
-    #     name="track_game",
-    #     description="Add a game to track deals and receive alerts."
-    # )
+    @commands.hybrid_command(name="game_deals", description="View or add tracked game deals")
     @app_commands.guilds(swancord)
-    async def track_game(self, interaction: discord.Interaction, message: discord.Message):
-        # Send the modal with an instance of our `GameTrackModal` class
-        # Since modals require an interaction, they cannot be done as a response to a text command.
-        # They can only be done as a response to either an application command or a button press.
-        await interaction.response.send_modal(GameTrackModal())
+    async def ask(self, ctx: commands.Context):
+        # We create the view and assign it to a variable so we can wait for it later.
+        view = GameDealHub()
+        await ctx.send('What do you want to do?', view=view)
+        # Wait for the View to stop listening for input...
+        await view.wait()
 
-    async def cog_unload(self) -> None:
-        self.bot.tree.remove_command(self.ctx_menu.name, type=self.ctx_menu.type)
+
+# Define a simple View that gives us a confirmation menu
+class GameDealHub(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+    # When the confirm button is pressed, set the inner value to `True` and
+    # stop the View from listening to more input.
+    # We also send the user an ephemeral message that we're confirming their choice.
+    @discord.ui.button(label="See Tracked Games", style=discord.ButtonStyle.green)
+    async def view_tracked_game(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message('Confirming', ephemeral=True)
+        self.stop()
+
+    # This one is similar to the confirmation button except sets the inner value to `False`
+    @discord.ui.button(label='Add New Game', style=discord.ButtonStyle.grey)
+    async def add_new_game(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_modal(GameTrackModal())
+        self.stop()
 
 
 async def setup(bot):
