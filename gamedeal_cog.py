94a5ff3ba354135545679cb8e8c3ff333731a880
Nick Swan <nickswan2624@gmail.com>
2024-06-13 13:22:55 -0400

    (WIP) Beginning stages of link parse. First menu looks good.

--- gamedeal_cog.py
+++ gamedeal_cog.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+import requests
 
 import traceback
 
@@ -24,11 +25,31 @@ class GameTrackModal(discord.ui.Modal, title="Track Game"):
         style=discord.TextStyle.long,
         placeholder='Insert Link',
         required=False,
-        max_length=300,
+        max_length=600,
     )
 
     async def on_submit(self, interaction: discord.Interaction):
-        # send steam store link
+        api_url = "https://www.cheapshark.com/api/1.0/deals?steamAppID="
+        store_link = self.feedback.value
+        print(store_link)
+        if "store.steampowered.com" not in store_link:
+            await interaction.response.send_message(f'Sorry, I did not recognize your Steam store link!', ephemeral=True)
+        else:
+            try:
+                # Parse for the steam app ID from the link then break
+                parse_link = str(store_link)
+                print(parse_link)
+                app_id = ""
+                for i in parse_link:
+                    if i.isdigit():
+                        while i.isdigit():
+                            app_id = app_id + i
+                        break
+                print(app_id)
+            except Exception as e:
+                print(e)
+                return
+
         # parse the steamAppID from the link to run through the cheapshark API
         # Return game deal with 2 buttons, close and track game
 
@@ -51,7 +72,7 @@ class GameDealCog(commands.Cog, name="GameDealCog"):
     async def ask(self, ctx: commands.Context):
         # We create the view and assign it to a variable so we can wait for it later.
         view = GameDealHub()
-        await ctx.send('What do you want to do?', view=view)
+        await ctx.send('Please select an option:', view=view, ephemeral=True)
         # Wait for the View to stop listening for input...
         await view.wait()
 
@@ -64,15 +85,21 @@ class GameDealHub(discord.ui.View):
     # When the confirm button is pressed, set the inner value to `True` and
     # stop the View from listening to more input.
     # We also send the user an ephemeral message that we're confirming their choice.
-    @discord.ui.button(label="See Tracked Games", style=discord.ButtonStyle.green)
+    @discord.ui.button(label="View Tracked Games", style=discord.ButtonStyle.green)
     async def view_tracked_game(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.send_message('Confirming', ephemeral=True)
         self.stop()
 
     # This one is similar to the confirmation button except sets the inner value to `False`
-    @discord.ui.button(label='Add New Game', style=discord.ButtonStyle.grey)
+    @discord.ui.button(label='Lookup/Add Game', style=discord.ButtonStyle.red)
     async def add_new_game(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.send_modal(GameTrackModal())
+        self.stop()
+
+    # todo: finish after cog is done.
+    @discord.ui.button(label='Help', style=discord.ButtonStyle.grey)
+    async def game_deal_help(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message('# I support markdown!\n To share a game from the steam client, scroll down to the Share button on the right', ephemeral=True)
         self.stop()
 
 
