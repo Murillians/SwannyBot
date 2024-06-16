b12f4d8c7b2f3b6b10994d6e60c7fe38edf78472
Nick Swan <shyguy2624@gmail.com>
2024-06-16 18:06:19 -0400

    Finished Game Lookup. Added Dropdown Example for Tracked Games List.

--- gamedeal_cog.py
+++ gamedeal_cog.py
@@ -8,16 +8,114 @@ import traceback
 
 import swannybottokens
 
+# Global active stores list
+stores_url = "https://www.cheapshark.com/api/1.0/stores"
+
+payload = {}
+headers = {}
+
+response = requests.request("GET", stores_url, headers=headers, data=payload)
+if response.status_code == 200:
+
+    stores_data = response.json()
+
+    stores_list = []
+    for store in stores_data:
+        stores_dict = {
+            'storeID': store['storeID'],
+            'storeName': store['storeName'],
+            'isActive': store['isActive'],
+        }
+        if stores_dict['isActive'] == 1:
+            stores_list.append(stores_dict)
+
 # The guild in which this slash command will be registered.
 swancord = discord.Object(swannybottokens.swancord)
 
 
-class GameTrackModal(discord.ui.Modal, title="Track Game"):
-    # # User enters title
-    # name = discord.ui.TextInput(
-    #     label='Title',
-    #     placeholder='Insert Game Title',
-    # )
+class GameDealCog(commands.Cog, name="GameDealCog"):
+
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.hybrid_command(name="game_deals", description="View or add tracked game deals")
+    @app_commands.guilds(swancord)
+    async def ask(self, ctx: commands.Context):
+        # We create the view and assign it to a variable so we can wait for it later.
+        view = GameDealHub()
+        await ctx.send('Please select an option:', view=view, ephemeral=True)
+        # Wait for the View to stop listening for input...
+        await view.wait()
+
+
+# Main Menu View that returns from slash command
+class GameDealHub(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+    # When the confirm button is pressed, set the inner value to `True` and
+    # stop the View from listening to more input.
+    # We also send the user an ephemeral message that we're confirming their choice.
+    @discord.ui.button(label="View Tracked Games", style=discord.ButtonStyle.green)
+    async def view_tracked_games(self, interaction: discord.Interaction, button: discord.ui.Button):
+        view = DropdownView()
+        await interaction.response.send_message("Select your username to view your tracked games:",
+                                                view=view, ephemeral=True)
+        self.stop()
+
+    # This one is similar to the confirmation button except sets the inner value to `False`
+    @discord.ui.button(label='Lookup/Track Game', style=discord.ButtonStyle.red)
+    async def lookup_game(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_modal(GameLookupModal())
+        self.stop()
+
+    # todo: finish after cog is done.
+    @discord.ui.button(label='Help', style=discord.ButtonStyle.grey)
+    async def game_deal_help(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message(
+            '# I support markdown!\n'
+            'To share a game from the steam client, scroll down to the Share button on the right\n',
+            ephemeral=True)
+        self.stop()
+
+
+# Dropdown List for "View Tracked Games" Button
+class DropdownView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+        # Adds the dropdown to our view object.
+        self.add_item(Dropdown())
+
+
+# Defines a custom Select containing colour options
+# that the user can choose. The callback function
+# of this class is called when the user changes their choice
+class Dropdown(discord.ui.Select):
+    def __init__(self):
+
+        # Set the options that will be presented inside the dropdown
+        options = [
+            discord.SelectOption(label='Red', description='Your favourite colour is red', emoji='🟥'),
+            discord.SelectOption(label='Green', description='Your favourite colour is green', emoji='🟩'),
+            discord.SelectOption(label='Blue', description='Your favourite colour is blue', emoji='🟦'),
+        ]
+
+        # The placeholder is what will be shown when no option is chosen
+        # The min and max values indicate we can only pick one of the three options
+        # The options parameter defines the dropdown options. We defined this above
+        super().__init__(placeholder='Select Game', min_values=1, max_values=1, options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        # Use the interaction object to send a response message containing
+        # the user's favourite colour or choice. The self object refers to the
+        # Select object, and the values attribute gets a list of the user's
+        # selected options. We only want the first one.
+        await interaction.response.send_message(f'Your favourite colour is {self.values[0]}', ephemeral=True)
+
+
+# Modal popup on "Lookup/Track Game" Button
+class GameLookupModal(discord.ui.Modal, title="Game Lookup"):
 
     # User enters link with max of 300 characters
     feedback = discord.ui.TextInput(
@@ -49,20 +147,56 @@ class GameTrackModal(discord.ui.Modal, title="Track Game"):
                     else:
                         id_hit = False
 
+                # Build request to cheapshark API
                 fixed_api_url = api_url + app_id
                 payload = {}
                 headers = {}
 
                 response = requests.request("GET", fixed_api_url, headers=headers, data=payload)
-                # Pretty Print JSON Formatter
                 parsed = json.loads(response.text)
-                deal_id = parsed[0]["dealID"]
-                print(json.dumps(parsed, indent=3))
 
-                fixed_cheapshark_link = cheapshark_link + deal_id
-                await interaction.response.send_message(fixed_cheapshark_link, ephemeral=True)
+                # Build the response message with all the stores available where the game is on sale
+                response_message = ""
+                on_sale_stores = ""
+                not_sale_stores = ""
+                title = ""
+                for i in parsed:
+                    deal_id = i["dealID"]
+                    store_id = i["storeID"]
+                    title = i["title"]
+                    sale_price = i["salePrice"]
+                    normal_price = i["normalPrice"]
+                    savings = round(float(i["savings"]))
+                    is_on_sale = int(i["isOnSale"])
 
-                # Return game deal with 2 buttons, close and track game
+                    store_name = ""
+                    for j in stores_list:
+                        if store_id == j["storeID"]:
+                            store_name = j["storeName"]
+                            break
+
+                    if is_on_sale == 1:
+                        on_sale_stores = (
+                                on_sale_stores +
+                                f"# [{store_name}](<{cheapshark_link}{deal_id}>) | **${sale_price}**\n"
+                                f"### ~~${normal_price}~~ | `-{savings}% OFF`\n"
+                        )
+                    elif is_on_sale == 0:
+                        not_sale_stores = (
+                                not_sale_stores +
+                                f"[{store_name}](<{cheapshark_link}{deal_id}>) | **${sale_price}** `MSRP`\n"
+                        )
+
+                response_message = (f"# __{title}__\n\n" + on_sale_stores + not_sale_stores +
+                                    "Waiting for a better deal? Track this game below now:\n")
+
+                # Pretty Print JSON Formatter
+                # print(json.dumps(parsed, indent=3))
+
+                view = ViewOnLookup()
+                await interaction.response.send_message(response_message, view=view, ephemeral=True)
+                # Wait for the View to stop listening for input...
+                await view.wait()
 
             except Exception as e:
                 print(e)
@@ -75,46 +209,14 @@ class GameTrackModal(discord.ui.Modal, title="Track Game"):
         traceback.print_exception(type(error), error, error.__traceback__)
 
 
-class GameDealCog(commands.Cog, name="GameDealCog"):
-
-    def __init__(self, bot: commands.Bot) -> None:
-        self.bot = bot
-
-    @commands.hybrid_command(name="game_deals", description="View or add tracked game deals")
-    @app_commands.guilds(swancord)
-    async def ask(self, ctx: commands.Context):
-        # We create the view and assign it to a variable so we can wait for it later.
-        view = GameDealHub()
-        await ctx.send('Please select an option:', view=view, ephemeral=True)
-        # Wait for the View to stop listening for input...
-        await view.wait()
-
-
-# Define a simple View that gives us a confirmation menu
-class GameDealHub(discord.ui.View):
+# Track Game Button after Game Lookup returns successful
+class ViewOnLookup(discord.ui.View):
     def __init__(self):
         super().__init__()
 
-    # When the confirm button is pressed, set the inner value to `True` and
-    # stop the View from listening to more input.
-    # We also send the user an ephemeral message that we're confirming their choice.
-    @discord.ui.button(label="View Tracked Games", style=discord.ButtonStyle.green)
-    async def view_tracked_game(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('Confirming', ephemeral=True)
-        self.stop()
-
-    # This one is similar to the confirmation button except sets the inner value to `False`
-    @discord.ui.button(label='Lookup/Add Game', style=discord.ButtonStyle.red)
-    async def add_new_game(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_modal(GameTrackModal())
-        self.stop()
-
-    # todo: finish after cog is done.
-    @discord.ui.button(label='Help', style=discord.ButtonStyle.grey)
-    async def game_deal_help(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message(
-            '# I support markdown!\n To share a game from the steam client, scroll down to the Share button on the right',
-            ephemeral=True)
+    @discord.ui.button(label="Track Game", style=discord.ButtonStyle.red)
+    async def track_game_on_lookup(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message('todo', ephemeral=True)
         self.stop()
 
 
