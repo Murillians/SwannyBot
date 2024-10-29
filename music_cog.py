5c3f68689ca454a1522a2b6d9b7fc2056f46daa8
Murillians <26067271+Murillians@users.noreply.github.com>
2024-10-29 10:55:54 -0400

    slight changes

--- music_cog.py
+++ music_cog.py
@@ -1,7 +1,7 @@
 import asyncio
 import datetime
 import os
-
+import pathlib
 import discord
 from discord.ext import commands
 
@@ -302,11 +302,19 @@ class MusicCog(commands.Cog):
         wavelink_player.queue.clear()
         await wavelink_player.disconnect()
 
+    @commands.command(name="leavetest")
     @commands.Cog.listener()
+
     async def on_wavelink_inactive_player(self, player: wavelink.Player) -> None:
         # Swanny Bot says "Bye Bye!" then disconnects
-        if os.path.exists("mario.mp3"):
-            tracks: wavelink.Playable = await wavelink.Playable.search("mario.mp3")
+        mariofile=pathlib.Path.cwd()/"mario.mp3"
+        if mariofile.exists():
+            tracks: wavelink.Playable = await wavelink.Playable.search(str(mariofile.as_posix()))
+            track: wavelink.Playable = tracks[0]
+            await player.play(track)
+            await asyncio.sleep(3)
+        else:
+            tracks: wavelink.Playable = await wavelink.Playable.search("https://youtu.be/F2Z2CklSxM0")
             track: wavelink.Playable = tracks[0]
             await player.play(track)
             await asyncio.sleep(3)
