fb485418babf80267a5c80fd115a93cce1a92774
shyguybros <nickswan2624@gmail.com>
2024-11-10 18:16:23 -0500

    swanny_bot.py cleanup. birthday table add. birthday cog finish (needs final task loop testing)

--- swanny_bot.py
+++ swanny_bot.py
@@ -3,18 +3,12 @@ import logging
 import discord
 from discord.ext import commands
 import swannybottokens
-from help_cog import help_cog
-from streamer_cog import streamer_cog
-from special_cog import special_cog
-from video_cog import video_cog
-#from rep_cog import rep_cog
-from twitterfixer_cog import twitterfixer_cog
-from music_cog import MusicCog
-from discord_fixer import DiscordFixer_cog
 import database
 import wavelink
 discord.utils.setup_logging()
-logger=logging.getLogger('discord')
+logger = logging.getLogger('discord')
+
+
 class Swannybot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
@@ -23,16 +17,17 @@ class Swannybot(commands.Bot):
         intents.members = True
         super().__init__(
         command_prefix=commands.when_mentioned_or('!'),
-        description="SwannyBot",
-        intents=intents,
-        help_command = None
+            description="SwannyBot",
+            intents=intents,
+            help_command=None
         )
 
     async def on_ready(self):
-        discordPresence = discord.Game("all your faves with !p")
-        await self.change_presence(activity=discordPresence)
+        discord_presence = discord.Game("all your faves with !p")
+        await self.change_presence(activity=discord_presence)
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('Bot is ready!')
+
     async def setup_hook(self) -> None:
         nodes = [wavelink.Node(uri="http://docker:2333", password=swannybottokens.WavelinkPassword,
                                inactive_player_timeout=600)]
@@ -47,11 +42,14 @@ class Swannybot(commands.Bot):
         await self.load_extension("special_cog")
         await self.load_extension("discord_fixer")
         await self.load_extension("gamedeal_cog")
+        await self.load_extension("birthday_cog")
         logger.info("All cogs loaded")
         logger.error("error logging works")
 
 
-swannybot :Swannybot= Swannybot()
+swannybot: Swannybot = Swannybot()
+
+
 async def main():
     async with swannybot:
         await swannybot.start(swannybottokens.discord_api_key)
